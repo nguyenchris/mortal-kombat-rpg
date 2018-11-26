d40f3f079b63c9015c72651422d0dd224fec6662
Chris Nguyen <34232894+nguyenchris@users.noreply.github.com>
2018-11-26 01:05:11 -0700

    added health bar css

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,26 +1,25 @@
 // https://78.media.tumblr.com/e0c55fc60b703206f5b50d61f08c66d1/tumblr_mgj1s8HP4A1rvbjw7o1_500.gif
 
-
-
-$(document).ready(function () {
+$(document).ready(function() {
   // show splash page once document is ready
-  $('.splash').show();
+  $(".splash").show();
 
-  var backgroundSong = new Audio('assets/audio/themesong.mp3');
-  backgroundSong.volume = 0.3;
-  backgroundSong.load();
+  // begin loading background music to prevent delay in playing;
+  var backgroundSong = new Audio("assets/audio/themesong.mp3");
+  backgroundSong.volume = 0.2;
+  backgroundSong.preload = "auto";
 
   // initalize game object
   mortalKombat = {
     characters: {
       subZero: {
-      name: "Sub-Zero",
-      health: 120,
-      attack: 8,
-      charImg: "assets/images/subzeroProf.png",
-      charGif: "assets/images/subzero.gif",
-      counterAttack: 15,
-      audio: ['assets/audio/subzero.wav', 'assets/audio/subzeroWins.wav']
+        name: "Sub-Zero",
+        health: 120,
+        attack: 8,
+        charImg: "assets/images/subzeroProf.png",
+        charGif: "assets/images/subzero.gif",
+        counterAttack: 15,
+        audio: ["assets/audio/subzero.wav", "assets/audio/subzeroWins.wav"]
       },
       scorpion: {
         name: "Scorpion",
@@ -29,7 +28,7 @@ $(document).ready(function () {
         charImg: "assets/images/scorpionProf.png",
         charGif: "assets/images/scorpion.gif",
         counterAttack: 5,
-        audio: ['assets/audio/scorpion.wav', 'assets/audio/scorpionWins.wav']
+        audio: ["assets/audio/scorpion.wav", "assets/audio/scorpionWins.wav"]
       },
       johnnyCage: {
         name: "Johnny Cage",
@@ -38,7 +37,10 @@ $(document).ready(function () {
         charImg: "assets/images/johnnycageProf.png",
         charGif: "assets/images/johnnycage.gif",
         counterAttack: 20,
-        audio: ['assets/audio/johnnycage.wav', 'assets/audio/johnnycageWins.wav']
+        audio: [
+          "assets/audio/johnnycage.wav",
+          "assets/audio/johnnycageWins.wav"
+        ]
       },
       raiden: {
         name: "Raiden",
@@ -47,85 +49,86 @@ $(document).ready(function () {
         charImg: "assets/images/raidenProf.png",
         charGif: "assets/images/raiden.gif",
         counterAttack: 25,
-        audio: ['assets/audio/raiden.wav', 'assets/audio/raidenWins.wav']
+        audio: ["assets/audio/raiden.wav", "assets/audio/raidenWins.wav"]
       }
     },
 
-    userChar: '',
+    userChar: "",
     userCharObj: {},
     opponentsArr: [],
     charactersObj: [],
-    opponentChar: '',
+    opponentChar: "",
     opponentCharObj: {},
     turnCounter: 1,
     killCounter: 0,
 
-
     // display character area for selection
     initializeCharacters: function() {
-      
       for (var key in this.characters) {
-
         var charDiv = $("<div class='playerChar' data-name='" + key + "'>");
-        var charName = $("<div class='character-name'>").text(this.characters[key].name);
-        var charImage = $("<img alt='image'>").attr("src", this.characters[key].charImg);
-        var charHealth = $("<div class='character-health'>").text('HP: ' + this.characters[key].health);
+        var charName = $("<div class='character-name'>").text(
+          this.characters[key].name
+        );
+        var charImage = $("<img alt='image'>").attr(
+          "src",
+          this.characters[key].charImg
+        );
+        var charHealth = $("<div class='character-health'>").text(
+          "HP: " + this.characters[key].health
+        );
 
-        charDiv.append(charName).append(charImage).append(charHealth);
-        charDiv.animateCss('zoomIn');
-        $('.playerMenu').append(charDiv);
+        charDiv
+          .append(charName)
+          .append(charImage)
+          .append(charHealth);
+        charDiv.animateCss("zoomIn");
+        $(".playerMenu").append(charDiv);
       }
 
       this.selectChar();
     },
 
-
-
     gameStart: function() {
-      // Check if usercharacter or opponent has been chosen 
+      // Check if usercharacter or opponent has been chosen
       if (this.userChar.length === 0 || this.opponentChar.length === 0) {
         // if not, select character
         this.selectChar();
       } else {
-        $('.playerMenu').hide();
+        $(".playerMenu").hide();
         backgroundSong.pause();
         this.renderCharacterVersus();
       }
     },
 
-
-
     // determines which characters are selected for user and opponent. Whichever is selected will be assigned to the properties userChar and opponent
     selectChar: function() {
-
       // hover animation effect over character options
-      $('.playerChar').mouseenter(function() {
-        $(this).animateCss('pulse');
-        $(this).css('animation-duration', '.8s');
+      $(".playerChar").mouseenter(function() {
+        $(this).animateCss("pulse");
+        $(this).css("animation-duration", ".8s");
         $(this).mouseleave(function() {
-          $('.playerChar').removeClass('animated pulse');
-        })
-      })
+          $(".playerChar").removeClass("animated pulse");
+        });
+      });
 
-      $('.playerChar').on('click', function() {
+      $(".playerChar").on("click", function() {
         var th = $(this);
 
-      // if there is not a character selected for the user..
+        // if there is not a character selected for the user..
         if (mortalKombat.userChar.length === 0) {
-          mortalKombat.updateCharacters('user', th.attr('data-name'));
+          mortalKombat.updateCharacters("user", th.attr("data-name"));
 
-          $(th).animateCss('zoomOutLeft', function() {
+          $(th).animateCss("zoomOutLeft", function() {
             mortalKombat.removeElement(th);
-            $('.directions').text('Select your Opponent');
+            $(".directions").text("Select your Opponent");
           });
-
         } else {
-          mortalKombat.updateCharacters('opponent', th.attr('data-name'));
+          mortalKombat.updateCharacters("opponent", th.attr("data-name"));
 
-          $(th).animateCss('zoomOutRight', function() {
+          $(th).animateCss("zoomOutRight", function() {
             mortalKombat.removeElement(th);
             mortalKombat.gameStart();
-          })
+          });
         }
       });
     },
@@ -139,13 +142,12 @@ $(document).ready(function () {
         // this.playAudioName(this.userCharObj.audio[0]);
       }
 
-      if (player === 'opponent') {
+      if (player === "opponent") {
         this.opponentChar = name;
         this.opponentCharObj = this.characters[name];
         // this.playAudioName(this.opponentCharObj.audio[0]);
       }
     },
-
 
     removeElement: function(el) {
       el.remove();
@@ -159,41 +161,47 @@ $(document).ready(function () {
       // backgroundSong.play();
     },
 
-    
     playAudioFight: function() {
-
-      var audio = new Audio('assets/audio/fight.wav')
+      var audio = new Audio("assets/audio/fight.wav");
       audio.play();
     },
 
-
-
     //  create character versus screen depending on character selections for both user and opponent
     renderCharacterVersus: function() {
-      
-      var vs = $("<img alt='image' class='versus'>").attr('src', 'assets/images/vs.png')
-      vs.animateCss('zoomInDown');
-      $('.vs').append(vs);
+      var vs = $("<img alt='image' class='versus'>").attr(
+        "src",
+        "assets/images/vs.png"
+      );
+      vs.animateCss("zoomInDown");
+      $(".vs").append(vs);
 
+      var newImgUser = $("<img alt='image' class='versus'>").attr(
+        "src",
+        this.userCharObj.charImg
+      );
+      newImgUser.animateCss("slideInLeft", function() {
+        var newImgOpp = $("<img alt='image' class='versus'>").attr(
+          "src",
+          mortalKombat.opponentCharObj.charImg
+        );
+        newImgOpp.animateCss("slideInRight", function() {
+          var fight = $("<img alt='image' class='versus'>").attr(
+            "src",
+            "assets/images/fight.png"
+          );
+          fight.animateCss("zoomIn", function() {
+            // after animation ends for the "fight" logo, render actual character fighter gif
+            mortalKombat.renderCharacterFighters();
+          });
 
-      var newImgUser = $("<img alt='image' class='versus'>").attr('src', this.userCharObj.charImg)
-      newImgUser.animateCss('slideInLeft', function() {
-        var newImgOpp = $("<img alt='image' class='versus'>").attr('src', mortalKombat.opponentCharObj.charImg)
-      newImgOpp.animateCss('slideInRight', function() {
-        var fight = $("<img alt='image' class='versus'>").attr('src', 'assets/images/fight.png')
-        fight.animateCss('zoomIn', function() {
-          // after animation ends for the "fight" logo, render actual character fighter gif
-          mortalKombat.renderCharacterFighters();
+          // display fight logo and play fight audio
+          $(".fight").append(fight);
+          mortalKombat.playAudioFight();
         });
-
-        // display fight logo and play fight audio
-        $('.fight').append(fight);
-        mortalKombat.playAudioFight();
+        $(".vsP2").append(newImgOpp);
+        mortalKombat.playAudioName(mortalKombat.opponentCharObj.audio[0]);
       });
-      $('.vsP2').append(newImgOpp)
-      mortalKombat.playAudioName(mortalKombat.opponentCharObj.audio[0]);
-      });
-      $('.vsP1').append(newImgUser)
+      $(".vsP1").append(newImgUser);
       this.playAudioName(this.userCharObj.audio[0]);
 
       // var newImgOpp = $("<img alt='image' class='versus'>").attr('src', this.opponentCharObj.charImg)
@@ -213,33 +221,39 @@ $(document).ready(function () {
       // $('.versusScreen').append(newDiv);
     },
 
-
-
-
     // render the selected user and opponent characters onto screen
     // also render attack button
     renderCharacterFighters: function() {
-      $('.versus').remove();
+      $(".versus").remove();
 
-      var userFighter = $("<img alt='image' id='p1'>").attr('src', this.userCharObj.charGif)
-      userFighter.animateCss('fadeIn');
-      $('.userArea').append(userFighter);
+      // display user selecter fighter
+      var userFighter = $("<img alt='image' id='p1'>").attr("src", this.userCharObj.charGif);
+      userFighter.animateCss("fadeIn");
+      $(".userArea").append(userFighter);
+
+      // display opponent fighter
+      var opponentFighter = $("<img alt='image' id='p2'>").attr("src", this.opponentCharObj.charGif);
+      opponentFighter.animateCss("fadeIn");
+      $(".opponentArea").append(opponentFighter);
 
       // create attack button
       var button = $('<button id="attack">');
-      button.text('Attack');
-      $('.userArea').append(button);
-      $(button).on('click', function() {
-        var th = $('#p1');
-        $(th).animate({"left": "40%"}, 1000);
-        $(th).animate({"left": "0px"}, 1000);
-        });
+      button.text("Attack");
+      $(".userArea").append(button);
+      $(button).on("click", function() {
+        var th = $("#p1");
+        $(th).animate({ left: "40%" }, 1000);
+        $(th).animate({ left: "0px" }, 1000);
+      });
+    },
+
+    // render the stats info above fight area to display health bar, logo, name, HP total
+    renderStats: function() {
+      
     }
-  }
+  };
 
-
-
-  //////////// This code will move images 
+  //////////// This code will move images
   // $("#p1").click(function () {
   //     var th = $(this);
   //     if ($(th).css('left') == '5px') {
@@ -258,125 +272,109 @@ $(document).ready(function () {
   //     }
   // });
 
+  //   $("#p2").click(function () {
+  //     var th = $(this);
+  //     if ($(th).css('left') == '5px') {
+  //         console.log("ret");
+  //         $(th).animate({
+  //             "left": "300px"
+  //         }, 3000);
+  //     } else {
+  //         console.log("sdffsdsff");
+  //         $(th).animate({
+  //             "right": "80%"
+  //         }, 1000);
+  //         $(th).animate({
+  //           "right": "0px"
+  //       }, 1000);
+  //     }
+  // });
 
-//   $("#p2").click(function () {
-//     var th = $(this);
-//     if ($(th).css('left') == '5px') {
-//         console.log("ret");
-//         $(th).animate({
-//             "left": "300px"
-//         }, 3000);
-//     } else {
-//         console.log("sdffsdsff");
-//         $(th).animate({
-//             "right": "80%"
-//         }, 1000);
-//         $(th).animate({
-//           "right": "0px"
-//       }, 1000);
-//     }
-// });
-
-
-
-// extend jQuery and set animateCss function in order to easily add animateCss classes after events
-// also will add functionality to where jquery will do something after an animation ends when setting a callback function
+  // extend jQuery and set animateCss function in order to easily add animateCss classes after events
+  // also will add functionality to where jquery will do something after an animation ends when setting a callback function
   $.fn.extend({
     animateCss: function(animationName, callback) {
       var animationEnd = (function(el) {
         var animations = {
-          animation: 'animationend',
-          OAnimation: 'oAnimationEnd',
-          MozAnimation: 'mozAnimationEnd',
-          WebkitAnimation: 'webkitAnimationEnd',
+          animation: "animationend",
+          OAnimation: "oAnimationEnd",
+          MozAnimation: "mozAnimationEnd",
+          WebkitAnimation: "webkitAnimationEnd"
         };
-  
+
         for (var t in animations) {
           if (el.style[t] !== undefined) {
             return animations[t];
           }
         }
-      })(document.createElement('div'));
-  
-      this.addClass('animated ' + animationName).one(animationEnd, function() {
-        $(this).removeClass('animated ' + animationName);
-  
-        if (typeof callback === 'function') callback();
+      })(document.createElement("div"));
+
+      this.addClass("animated " + animationName).one(animationEnd, function() {
+        $(this).removeClass("animated " + animationName);
+
+        if (typeof callback === "function") callback();
       });
-  
+
       return this;
-    },
+    }
   });
-
-
 
   // click event listener for play button to display animations and remove splash page
-  $('.splash-btn').on('click', function() {
+  $(".splash-btn").on("click", function() {
     // display animations
     $(this).css({
-      'animation-delay': '0s',
-      'animation-duration': '1s'
+      "animation-delay": "0s",
+      "animation-duration": "1s"
     });
 
-    $('.splash-img').css({
-      'animation-duration': '1s',
-      'animation-delay': '0s'
+    $(".splash-img").css({
+      "animation-duration": "1s",
+      "animation-delay": "0s"
     });
 
-    $(this).animateCss('zoomOut');
+    $(this).animateCss("zoomOut");
 
-    $('.splash-img').animateCss('fadeOutUp', function() {
+    $(".splash-img").animateCss("fadeOutUp", function() {
       // remove entire splash page
-      removeSplash('.splash'); 
-
+      removeSplash(".splash");
     });
   });
-
 
   // removes splash page and displays game page
   function removeSplash(el) {
     $(el).remove();
     // show game page
-    $('.game-page').show();
-    
+    $(".game-page").show();
+
     backgroundSong.play();
 
-
     mortalKombat.initializeCharacters();
-
   }
 
-
-//   $("#p1").click(function () {
-//     var th = $(this);
-//     if ($(th).css('left') == '5px') {
-//         console.log("ret");
-//         $(th).animate({
-//             "left": "300px"
-//         }, 3000);
-//     } else {
-//         console.log("sdffsdsff");
-//         $(th).animate({
-//             "left": "80%"
-//         }, 1000);
-//         $(th).animate({
-//           "left": "0px"
-//       }, 1000);
-//     }
-// });
-
-
+  //   $("#p1").click(function () {
+  //     var th = $(this);
+  //     if ($(th).css('left') == '5px') {
+  //         console.log("ret");
+  //         $(th).animate({
+  //             "left": "300px"
+  //         }, 3000);
+  //     } else {
+  //         console.log("sdffsdsff");
+  //         $(th).animate({
+  //             "left": "80%"
+  //         }, 1000);
+  //         $(th).animate({
+  //           "left": "0px"
+  //       }, 1000);
+  //     }
+  // });
 
   function hideInsert(el) {
     el.hide();
-
 
     // insert selected character and display in fight area for user
     // var newDiv = $('<div class="p1"><img src="assets/images/scorpion.gif" id="p1" alt=""></div>')
     // newDiv.animateCss('fadeIn');
     // $('.userArea').append(newDiv);
-
   }
-
-
 });
